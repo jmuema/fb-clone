67b847fc2a92e4c176029184b59c56bda8b7e292
muema254 <jojotheone7@gmail.com>
2020-09-27 13:11:15 +0300

    created the header component

---
+++ src/Header.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsOutlinedIcon from '@material-ui/icons/Subscriptions';
+import StorefrontOutlinedIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar, IconButton } from '@material-ui/core';
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+
+
+
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://iconape.com/wp-content/files/yd/117914/svg/Facebook_f_logo__2019_.svg" alt="" />
+                <div className="header__input">
+                    <SearchIcon />
+                    <input type="text" />
+
+                </div>
+                </div>
+                <div className="header__center">
+                    <div className="header__option">
+                        <HomeIcon fontsize="large" />
+
+                    </div>
+                    <div className="header__option">
+                        <FlagIcon fontsize="large" />
+
+                    </div>
+                    <div className="header__option">
+                        <SubscriptionsOutlinedIcon fontsize="large" />
+
+                    </div>
+                    <div className="header__option">
+                        <StorefrontOutlinedIcon fontsize="large" />
+
+                    </div>
+                    <div className="header__option">
+                        <SupervisedUserCircleIcon fontsize="large" />
+
+                    </div>
+                    </div>
+                    <div className="header__right">
+                        <div className="header__info">
+                            <Avatar/>   
+                            <h4>Ulemkamba</h4>
+                            <IconButton>
+                                <AddIcon/>
+                            </IconButton>
+                            <IconButton>
+                                <ForumIcon/>
+                            </IconButton>
+                            <IconButton>
+                                <NotificationsActiveIcon/>
+                            </IconButton>
+                            <IconButton>
+                                <ExpandMoreIcon/>
+                            </IconButton>
+
+                        </div>
+
+                    </div>
+
+
+                
+
+        
+
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5Rh3I6-3j5_kPMDdvsVCvMRhzbIxgA7U",
+    authDomain: "facebook-clone-12fcb.firebaseapp.com",
+    databaseURL: "https://facebook-clone-12fcb.firebaseio.com",
+    projectId: "facebook-clone-12fcb",
+    storageBucket: "facebook-clone-12fcb.appspot.com",
+    messagingSenderId: "284295590687",
+    appId: "1:284295590687:web:6bb24af31f807a2cfeb62a",
+    measurementId: "G-BFTB35YB3K"
+  };
