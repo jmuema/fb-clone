4fbe65b42e70441bdd6d677d0bc313a94f003fdd
muema254 <jojotheone7@gmail.com>
2020-09-28 09:03:19 +0300

    created the feed component

---
+++ src/Feed.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Feed.css';
+import StoryReel from "./StoryReel";
+import MessageSender from "./MessageSender";
+
+function Feed() {
+    return (
+        <div className="feed" >
+            {/* StoryReel */}
+            <StoryReel/>
+
+            {/* MessageSender */}
+            <MessageSender/>
+            
+        </div>
+    )
+}
+
+export default Feed
