1e300dded536adca444c6548992c5051b78bedb2
muema254 <jojotheone7@gmail.com>
2020-09-28 09:04:42 +0300

    added the story reel component

---
+++ src/StoryReel.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './StoryReel.css';
+import Story from "./Story";
+
+
+function StoryReel() {
+    return (
+        <div className="storyReel" >
+            {/* Story */}
+            <Story 
+            image="https://media.firstcoastnews.com/assets/WTLV/images/5c881363-09ae-4e8f-be14-2b7a6b85f70a/5c881363-09ae-4e8f-be14-2b7a6b85f70a_750x422.jpg"
+            profileSrc="https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png"
+            title="Joseph Muema"
+            
+            />
+            <Story 
+            image="https://ruinmyweek.com/wp-content/uploads/2019/12/auto-draft-6.png"
+            profileSrc="https://tunisaid.org/wp-content/uploads/2019/03/avataaars-2.png"
+            title="James Sila"
+            
+            />
+            <Story 
+            image="https://st1.latestly.com/wp-content/uploads/2020/06/2020-06-10-3-380x214.jpg"
+            profileSrc="https://techharvest.asia/wp-content/uploads/2018/06/avataaars-2-528x430.png"
+            title="David Mwema"
+            
+            />
+            <Story 
+            image="https://m.hindustantimes.com/rf/image_size_960x540/HT/p2/2020/08/06/Pictures/_3154e842-d7c7-11ea-a162-aa5ffaaa8aa4.jpg"
+            profileSrc="https://www.businesstyc.com/wp-content/uploads/2019/03/avataaars.png"
+            title="Alice Waithera"
+            
+            />
+            <Story 
+            image="https://greenlemon.me/wp-content/uploads/2020/05/3-50.jpg"
+            profileSrc="https://lh3.googleusercontent.com/proxy/ZZz1hJVrk7eek9rakmN-KDvGEkos9-3eNNDq8-V6jW-i2uRCRZtV2nLDEyfVtoq3pc7Kw9Ruioovuyp45azGMF9_zzJg52rIb2KIs-vTdePe7CjWQIP4O0I"
+            title=" Stacy Wanjiru"
+            
+            />
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+            {/* Story */}
+        </div>
+    )
+}
+
+export default StoryReel
